9f212b50cff45cff3d2db3f64d4b8d89591bfcf1
Russell Klenk <contact@russellklenk.com>
2016-02-14 12:04:48 -0800

    Added win32_random.cc module.

--- src/win32_client.cc
+++ src/win32_client.cc
@@ -43,6 +43,7 @@
 #include "win32_runtime.cc"
 #include "win32_memarena.cc"
 #include "win32_timestamp.cc"
+#include "win32_random.cc"
 #include "win32_scheduler.cc"
 #include "win32_parse.cc"
 #include "win32_display.cc"
